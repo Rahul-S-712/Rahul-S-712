a87f6e795477bb0ffdeb937d4bb2e36f14950b47
Rahul-S-712 <146565051+Rahul-S-712@users.noreply.github.com>
2023-09-30 22:45:35 +0530

    Update Python_Day_1.py

--- Python_Day_1.py
+++ Python_Day_1.py
@@ -1,1 +1,12 @@
+# 100 Days of Code - Python
+# Day 1
+# Date :- 30/09/2023
 
+# Project Title :- "Band Name Generator"
+
+print("Welcome to the Band Name Generator\n")
+
+name_of_city = input("What is the name of the city that you grew up in ?\n")
+name_of_pet = input("What is your pet's name ?\n")
+
+print("Your band name could be " + name_of_city + " " + name_of_pet)
