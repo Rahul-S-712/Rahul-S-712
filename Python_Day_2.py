db1c7d297c7f768a5bc67122ddedfbf4e2164081
Rahul-S-712 <146565051+Rahul-S-712@users.noreply.github.com>
2023-10-01 19:14:56 +0530

    Update Python_Day_2.py

--- Python_Day_2.py
+++ Python_Day_2.py
@@ -2,4 +2,20 @@
 # Day 2
 # Date :- 01/10/2023
 
-# Project Title :- ""
+# Project Title :- "Bill Splitter with Tip Calculator In-Built"
+
+print("Welcome to the Bill Splitter.\n")
+bill = input("How much was the total bill ? $")
+tip = input("\nWhat percentage tip would you like to give?\n7%, 10%, 12%, or 15%?\n(Enter only the number) : ")
+ppl = input("\nHow many people to split the bill between ? ")
+
+bill = float(bill)
+tip = int(tip)
+ppl = int (ppl)
+
+split_amount = bill/ppl
+tip_amount = ((tip/100)*bill)
+final_split_amount = (split_amount + (tip_amount/ppl))
+split = round(final_split_amount,2)
+
+print(f"\nEach person should pay ${split}")
